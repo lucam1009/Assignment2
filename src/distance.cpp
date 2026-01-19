cdaaed56815874bb473a5808d2415bbef5c0f387
lucam1009 <lucamm1009@gmail.com>
2026-01-19 16:50:55 +0100

    assignment with new topics for exam

---
+++ src/distance.cpp
@@ -0,0 +1,150 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include <iostream>
+#include <string>
+#include <limits>
+#include "std_msgs/msg/int32.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "assignment2_msgs/srv/threshold.hpp"
+#include "assignment2_msgs/srv/average.hpp"
+#include <cmath>
+#include "assignment2_msgs/msg/obstacle_position.hpp"
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+using Threshold = assignment2_msgs::srv::Threshold;
+using Average = assignment2_msgs::srv::Average;
+using Obstacle_position = assignment2_msgs::msg::ObstaclePosition;
+
+
+class distance_check: public rclcpp::Node
+{
+    public:
+    distance_check(): Node("distance_check")
+    {
+        sub_scan = this->create_subscription<sensor_msgs::msg::LaserScan>("/laser_scan", 10, std::bind(&distance_check::callback_scan, this, _1));
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(50), std::bind(&distance_check::control_loop, this));
+        sub_position = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, std::bind(&distance_check::callback_position, this, _1));
+        pub_safety = this->create_publisher<std_msgs::msg::Int32>("/safety_status", 10);
+        pub_emergency_vel = this->create_publisher<geometry_msgs::msg::Twist>("/robot_vel", 10);
+        server = this->create_service<Threshold>("generate_threshold", std::bind(&distance_check::handle_service, this, _1, _2));
+        pub_obstacle = this->create_publisher<assignment2_msgs::msg::ObstaclePosition>("/obstacle", 10);
+        server_average = this->create_service<Average>("generate_average", std::bind(&distance_check::handle_average, this, _1, _2));
+
+
+    }
+
+    float angle, ostacolo_x, ostacolo_y, distance;
+    bool emergency_mode = false;
+    float min_distance = 100.0;
+    float current_threshold = 1.5;
+    float current_lin_average = 0.0;
+    float current_ang_average = 0.0;
+    geometry_msgs::msg::Pose current_pose;
+    
+    private:
+    
+    void callback_position(const nav_msgs::msg::Odometry::SharedPtr msg)
+    {
+        current_pose = msg->pose.pose;
+    }
+
+    void callback_scan(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+    {
+        int n_ranges = msg->ranges.size();
+        float min_distance_loc = 100.0;
+        int valid_points = 0;
+        int index = 0;
+        for(int i = 0; i < n_ranges; i++){
+            float distance = msg->ranges[i];
+            bool check = std::isfinite(distance) && distance > msg->range_min && distance < msg->range_max;
+            if(check){
+                valid_points++;
+                if (distance < min_distance_loc){
+                    min_distance_loc = distance;
+                    index = i;
+                }
+            }
+        }
+        if(valid_points > 0){
+            min_distance = min_distance_loc;
+            float angle = msg->angle_min + (index * msg->angle_increment);
+            float ostacolo_x = min_distance * std::cos(angle);
+            float ostacolo_y = min_distance * std::sin(angle);
+
+            assignment2_msgs::msg::ObstaclePosition ob_msg;
+            ob_msg.distance = min_distance;
+            ob_msg.pos_x = ostacolo_x;
+            ob_msg.pos_y = ostacolo_y;
+            ob_msg.threshold = current_threshold;
+            pub_obstacle->publish(ob_msg);
+        }else
+            min_distance = 100.0;
+    }
+
+
+    void control_loop()
+    {
+        std_msgs::msg::Int32 status;
+        geometry_msgs::msg::Twist emergency_cmd;
+
+        if (min_distance < current_threshold){
+            if(!emergency_mode){
+                RCLCPP_INFO(this->get_logger(), "Obstacle too close, go back to safe position");
+                emergency_mode = true;
+            }
+        }else if(min_distance > (current_threshold+0.5)){
+            if(emergency_mode){
+                emergency_mode = false;
+            }
+        }
+
+        if(emergency_mode){
+            status.data = 1;
+
+            emergency_cmd.linear.x = -1.0;
+            emergency_cmd.angular.z = 0.0;
+            pub_emergency_vel->publish(emergency_cmd);
+
+        }else{
+            status.data = 0;
+        }
+
+        pub_safety->publish(status);
+    }
+
+    void handle_service(const std::shared_ptr<Threshold::Request> req, std::shared_ptr<Threshold::Response> res){
+        current_threshold = req->threshold;
+        res->x = current_threshold;
+        RCLCPP_INFO(this->get_logger(), "New threshold: %.2f", current_threshold);       
+    }
+
+    void handle_average(const std::shared_ptr<Average::Request> req, std::shared_ptr<Average::Response> res){
+        current_lin_average = req->lin_average;
+        current_ang_average = req->ang_average;
+        res->lin_average = current_lin_average;
+        res->ang_average = current_ang_average;
+        RCLCPP_INFO(this->get_logger(), "New lin average: %.2f,\n new ang average: %.2f", current_lin_average, current_ang_average); 
+    }
+
+
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub_scan;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_position;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr pub_safety;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_emergency_vel;
+    rclcpp::TimerBase::SharedPtr threshold_timer_;
+    rclcpp::Service<Threshold>::SharedPtr server;
+    rclcpp::Service<Average>::SharedPtr server_average;
+    rclcpp::Publisher<assignment2_msgs::msg::ObstaclePosition>::SharedPtr pub_obstacle;
+
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<distance_check>());
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ src/movement.cpp
@@ -0,0 +1,220 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include "assignment2_msgs/srv/threshold.hpp"
+#include "assignment2_msgs/srv/average.hpp"
+#include "assignment2_msgs/msg/obstacle_position.hpp"
+#include <iostream>
+#include <limits>
+#include <thread>
+#include <chrono>
+#include <deque>
+
+using std::placeholders::_1;
+using Threshold = assignment2_msgs::srv::Threshold;
+using Average = assignment2_msgs::srv::Average;
+
+class movement: public rclcpp::Node
+{
+    public:
+    movement(): Node("movement")
+    {
+        pub_velocity = this->create_publisher<geometry_msgs::msg::Twist>("/robot_vel", 10);
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(50), std::bind(&movement::execute, this));
+        sub_safety = this->create_subscription<std_msgs::msg::Int32>("/safety_status", 10, std::bind(&movement::callback_safety, this, _1));
+        threshold_client = this->create_client<Threshold>("generate_threshold");
+        sub_obstacle = this->create_subscription<assignment2_msgs::msg::ObstaclePosition>("/obstacle", 10, std::bind(&movement::callback_obstacle,this, _1));
+        average_client = this->create_client<Average>("generate_average");
+
+        std::thread(&movement::user_input_loop, this).detach();
+    }
+
+    double v = 0.0;
+    double alpha = 0.0;
+    int safety_status = 0;
+    float distance, pos_x, pos_y, threshold;
+    std::deque<float> lin_vel;
+    std::deque<float> ang_vel;
+    float lin_average = 0.0;
+    float ang_average = 0.0;
+    geometry_msgs::msg::Twist message;
+
+    private:
+
+    void user_input_loop(){
+        int choice;
+        while(rclcpp::ok()){
+            std::cout << "Insert 1 for change the velocity, 2 for the threshold or 3 for the average of the velocities: ";
+            std::cin >> choice;
+            
+            if (std::cin.fail()) {
+            std::cout << "Error: You entered a character instead of a number. Please try again.\n";
+            std::cin.clear(); 
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+            continue;
+            }
+
+            if(choice == 1 || choice == 2){
+                std::cout << "The closest obstacle is at a distance: " << distance << "\n at position:\n x = " <<pos_x<< "\n y = " <<pos_y<< "\n with threshold: "<<threshold<< std::endl;
+            }
+            if (!std::cin.fail()) {
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                if(choice == 1){
+                    get_user_input();
+                } else if(choice == 2){
+                    float new_threshold;
+                    std::cout << "insert the new threshold: ";
+                    std::cin >> new_threshold;
+                    if(!std::cin.fail()) {
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        call_threshold_service(new_threshold);
+                    } else {
+                        std::cout << "Invalid threshold input.\n";
+                        std::cin.clear();
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    }
+                }else if(choice == 3){
+                    float new_average_lin = lin_average;
+                    float new_average_ang = ang_average;
+                    call_average_service(new_average_lin, new_average_ang);               
+                }else {
+                    std::cout << "Error: insert a number." << '\n';
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+            }
+        }
+    }
+
+    void call_threshold_service(float val){
+        if(!threshold_client->wait_for_service(std::chrono::seconds(1))){
+            std::cout << "Error, server threshold not responding\n";
+            return;
+        }
+
+        auto request = std::make_shared<Threshold::Request>();
+        request->threshold = val;
+
+        threshold_client->async_send_request(request);
+    }
+
+        void call_average_service(float lin_average, float ang_average){
+        if(!average_client->wait_for_service(std::chrono::seconds(1))){
+            std::cout << "Error, server average not responding\n";
+            return;
+        }
+
+        auto request = std::make_shared<Average::Request>();
+        request->lin_average = lin_average;
+        request->ang_average = ang_average;
+
+        average_client->async_send_request(request);
+        std::cout << ">>> Request sent! Linear average: " << lin_average << ", Angular average: " << ang_average << std::endl;
+    }
+
+    void execute(){
+        if(safety_status == 1){
+            return;
+        } else {
+            message.linear.x = v;
+            message.angular.z = alpha;
+        }
+        pub_velocity->publish(message);
+    }
+    
+    void stop(){
+        message.linear.x = 0.0;
+        message.angular.z = 0.0;
+        pub_velocity->publish(message);
+    }
+
+void get_user_input() {
+        double temp_v = 0.0;
+        double temp_alpha = 0.0;
+
+        while (true) {
+            std::cout << "Insert the linear velocity: ";
+            std::cin >> temp_v;
+            if (!std::cin.fail()) {
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                break; 
+            }
+            std::cout << "Error: insert a number." << '\n';
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+
+        while (true) {
+            std::cout << "Insert the angular velocity: ";
+            std::cin >> temp_alpha;
+            if (!std::cin.fail()) {
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                break; 
+            }
+            std::cout << "Error: insert a number." << '\n';
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+
+        v = temp_v;
+        alpha = temp_alpha;
+        lin_vel.push_back(v);
+        ang_vel.push_back(alpha);
+        float lin_sum = 0.0;
+        float ang_sum = 0.0;
+        if(lin_vel.size() == 5 || ang_vel.size() == 5){
+            for(int i = 0; i < 5; i++){
+                lin_sum += lin_vel[i];
+                ang_sum += ang_vel[i];
+            }
+            lin_average = lin_sum/5;
+            ang_average = ang_sum/5;
+            lin_vel.pop_front();
+            ang_vel.pop_front();
+        }
+
+        for(int i = 0; i < 20; i++){
+            if(safety_status == 1) {
+                break; 
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(200)); //aggiungo per far sì che il movimento duri solo per 4 secondi
+        }
+
+        v = 0.0;
+        alpha = 0.0;
+        stop(); 
+
+    }
+
+    void callback_safety(const std_msgs::msg::Int32::SharedPtr msg)
+    {
+        safety_status = msg->data;
+        if(safety_status == 1){
+            v = 0.0;
+            alpha = 0.0;
+        }
+    }
+
+    void callback_obstacle(const assignment2_msgs::msg::ObstaclePosition::SharedPtr obstacle)
+    {
+        distance = obstacle->distance;
+        pos_x = obstacle->pos_x;
+        pos_y = obstacle->pos_y;
+        threshold = obstacle->threshold;
+    }
+
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_velocity;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sub_safety;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Client<Threshold>::SharedPtr threshold_client;
+    rclcpp::Client<Average>::SharedPtr average_client;
+    rclcpp::Subscription<assignment2_msgs::msg::ObstaclePosition>::SharedPtr sub_obstacle;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<movement>());
+    rclcpp::shutdown();
+    return 0;
+}
